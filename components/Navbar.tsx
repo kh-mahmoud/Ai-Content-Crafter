cda2cceacce05ebcaeadc3ce50cf670ca4dea60b
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2025-08-14 00:50:57 +0200

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,9 @@
-
 import { SidebarTrigger } from "./ui/sidebar";
 import NavItems from "./NavItems";
 
-
 const Navbar = () => {
   return (
-    <nav className="flex justify-between w-full p-4 items-center">
+    <nav className="justify-items-between sticky top-0 z-10 w-full p-4 bg-white/70 backdrop-blur-3xl">
       <SidebarTrigger className="cursor-pointer" />
       <NavItems />
     </nav>
