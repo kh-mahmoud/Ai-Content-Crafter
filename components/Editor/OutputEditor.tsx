ac7074319a64e9807d7d1f89b3c8cd4626f831a5
Khouchane Mahmoud <97807779+kh-mahmoud@users.noreply.github.com>
2025-09-06 18:36:08 +0200

    Update OutputEditor.tsx

--- components/Editor/OutputEditor.tsx
+++ components/Editor/OutputEditor.tsx
@@ -47,7 +47,7 @@ const OutputEditor = ({ editorData }: outputEditorProps) => {
     }
 
     // --- Title request ---
-    const titleResponse = await fetch("process.env.NEXT_PUBLIC_WORKER", {
+    const titleResponse = await fetch(process.env.NEXT_PUBLIC_WORKER, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -60,7 +60,7 @@ const OutputEditor = ({ editorData }: outputEditorProps) => {
     const newTitle = titleJson.result;
 
     // --- Description request ---
-    const descResponse = await fetch("process.env.NEXT_PUBLIC_WORKER", {
+    const descResponse = await fetch(process.env.NEXT_PUBLIC_WORKER, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
